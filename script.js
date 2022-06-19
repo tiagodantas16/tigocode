326220d50b2e1c8cdb34add8ee269e54764aa859
Tiago Pereira Dantas <devdantas@pop-os.localdomain>
2022-06-19 02:52:30 -0300

    mobile version development

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const header = document.querySelector('header');
 const logo = document.querySelectorAll('.logo')[0]
 const navBar = document.querySelectorAll('.nav-bar')[0]
+const menu = document.querySelectorAll('.menu')[0]
+let isOpen = false;
 
-window.addEventListener('scroll', toggleHeader, false);
+window.addEventListener('scroll', toggleHeader, false)
+menu.addEventListener('click', toggleMenu, false)
+navBar.addEventListener('click', navClick, false)
 
 function toggleHeader() {
   if (window.pageYOffset > 60 && header.classList.contains('max-header')) {
@@ -19,5 +23,21 @@ function toggleHeader() {
     logo.classList.add('max-logo')
     navBar.classList.remove('min-nav')
     navBar.classList.add('max-nav')
+  }
+}
+function toggleMenu() {
+  if (!isOpen){
+    navBar.classList.add('menu-opened')
+    menu.firstElementChild.classList.add('close-btn')
+    isOpen = true;
+  } else {
+    navBar.classList.remove('menu-opened')
+    menu.firstElementChild.classList.remove('close-btn')
+    isOpen = false;
+  }
+}
+function navClick(evt) {
+  if (evt.target.tagName === 'A') {
+    toggleMenu()
   }
 }
